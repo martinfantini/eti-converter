d260c21e2951579014324ff14c18b7a7f4796b0c
Roberto Martin Fantini <martin.fantini@gmail.com>
2025-08-17 10:13:24 +0200

    Update the options from the main menu

--- app/__main__.py
+++ app/__main__.py
@@ -6,13 +6,15 @@ import traceback
 import sys
 from argparse import ArgumentParser, SUPPRESS
 from app.parser import Parser
+from app.schema import *
 
 def main() -> None:
     parser = ArgumentParser(prog='eti-converter-gen', description='ETI codec generator')
     parser.add_argument('--schema', help='path to xml schema', required=True)
     parser.add_argument('--destination', help='path to directory where codec will be written', required=True)
-    parser.add_argument('--generator', help='choose generator (available: cpp, rust, python)', default='cpp')
-    parser.add_argument('--package', help='override model mane property')
+    parser.add_argument('--generator', help='choose generator (available: cpp, cppns or rust)', default='cpp', type=str)
+    parser.add_argument('--package', help='override model name property', default='', type=str)
+    parser.add_argument('--byteorder', help='override byte order, default littleEndian (available: littleEndian, bigEndian)', default='littleEndian', type=str)
 
     args = parser.parse_args()
 
@@ -20,8 +22,12 @@ def main() -> None:
         module = importlib.import_module(f'app.generation.{args.generator}')
         Generator = getattr(module, 'Generator')
         schema = Parser.from_file(args.schema).get_schema()
+        if args.byteorder == 'bigEndian':
+           schema.byte_order = ByteOrder.BIG_ENDIAN
+        if len(args.package) != 0:
+            schema.name = args.package
         generator = Generator(args.destination)
-        generator.generate(schema, package=args.package)
+        generator.generate(schema)
     except Exception as e:
         sys.exit(traceback.format_exc())
         sys.exit(f'error: {e}')
